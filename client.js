bca9b29ad583aeb895fc98b5760bcfc5e06a4dd2
Howard Yeh <howard@metacircus.com>
2015-10-09 18:05:00 +0800

    add client

---
+++ client.js
@@ -0,0 +1,62 @@
+let renderMarkdown = require("./render");
+let React = require("react");
+
+// console.log(snakeCase);
+
+// setInterval(() => {
+//   console.log(new Date());
+// },1000)
+
+// function connect() {
+//   let sockjs = new SockJS('/echo');
+
+
+//   sockjs.onopen = () => {
+//     console.log('[*] open', sockjs.protocol);
+//   };
+
+//   sockjs.onmessage = function(e) {
+//     console.log('[.] message', e.data);
+//   };
+
+//   sockjs.onclose   = function()  {
+//     console.log('[*] close');
+//   };
+// }
+
+let App = React.createClass({
+  getInitialState() {
+    return {
+      sections: [],
+    };
+  },
+
+  componentDidMount() {
+    let sockjs = new SockJS('/echo');
+
+    sockjs.onopen = () => {
+      console.log('[*] open', sockjs.protocol);
+    };
+
+    sockjs.onmessage = (e) => {
+
+      let sections = JSON.parse(e.data);
+      console.log('sections:', sections);
+      this.setState({sections});
+    };
+
+    sockjs.onclose   = function()  {
+      console.log('[*] close');
+    };
+  },
+
+  render() {
+    let {sections} = this.state;
+    let doc = renderMarkdown(sections)
+    return doc;
+  },
+});
+
+window.onload = () => {
+  React.render(<App/>, document.body);
+};
