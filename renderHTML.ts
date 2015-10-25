a6dba2c10ad6184eb19294a559adb8433b3aceee
Howard Yeh <howard@metacircus.com>
2015-10-25 11:53:55 +0800

    rename renderHTML

---
+++ renderHTML.ts
@@ -0,0 +1,28 @@
+let babel = require("babel-core/browser");
+
+import * as React from "react";
+
+module.exports = function renderHTML(html:string,widgets={}): React.HTMLElement {
+  let jsx = `let dom = ${html}`;
+
+  // console.log("jsx",jsx);
+
+  let code = babel.transform(jsx).code;
+
+  // console.log("code",jsx);
+
+  let widgetNames = Object.keys(widgets);
+  let body = `${code}; return dom;`;
+  let args = ["React",...widgetNames];
+  let widgetValues = widgetNames.map(name => widgets[name]);
+
+  // console.log("args",widgets);
+  // console.log("args",args);
+  // console.log("values",widgetValues);
+
+  let render = new Function(...args,body);
+
+  let dom = render(React,...widgetValues);
+
+  return dom;
+}
